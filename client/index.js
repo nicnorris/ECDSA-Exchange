112f69f3a70255feba196c3255eda5d746b53ec8
nicnorris <nicnorrismail@gmail.com>
2021-07-19 11:37:08 -0700

    update client to sign transaction

--- client/index.js
+++ client/index.js
@@ -2,6 +2,11 @@ import "./index.scss";
 
 const server = "http://localhost:3042";
 
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
+const ec = new EC('secp256k1');
+
+
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
   if(value === "") {
     document.getElementById("balance").innerHTML = 0;
@@ -16,12 +21,18 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const privateKey = document.getElementById("private-key").value;
+  const transaction = {
+    amount, recipient
+  }
+  const key = ec.keyFromPrivate(privateKey, 'hex');
+  const signature = key.sign(SHA256(JSON.stringify(transaction)).toString());
+
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    transaction, signature: signature.toDER('hex'), publicKey: key.getPublic().encode('hex')
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
